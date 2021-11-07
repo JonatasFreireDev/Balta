ece0ebba642ac19fe4b314e9c1fb918c4a9e2c44
Jonatas <jonatasf1997@hotmail.com>
2021-11-07 18:08:23 -0300

    init project

---
+++ 2 - Orientação a Objetos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2___Orientação_a_Objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
